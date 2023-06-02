85f83b8687237ce0f567e151abb735558a8fbb7a
calvin <calvinsamuel04@gmail.com>
2023-06-02 10:16:40 +0900

    fixed java

--- app/src/main/java/com/example/grouphub/profile.java
+++ app/src/main/java/com/example/grouphub/profile.java
@@ -9,6 +9,6 @@ public class profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_placeholder);
+        setContentView(R.layout.profile);
     }
 }
